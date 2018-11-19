dccc48fd13f3e42ae7397ec432e2eedf9f271c22
s25419 <siwakornanunaua@gmail.com>
2018-11-19 10:04:54 +0700

    finish

---
+++ app/src/main/java/com/example/admin/ass09_kotlin/Student.kt
@@ -0,0 +1,42 @@
+package com.example.admin.ass09_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+
+class Student(val Username: String, val Password: String , val Birthday: String, val Gender: String,  val Email: String, val Phone: String): Parcelable{
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(Username)
+        parcel.writeString(Password)
+        parcel.writeString(Birthday)
+        parcel.writeString(Gender)
+        parcel.writeString(Email)
+        parcel.writeString(Phone)
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Student> {
+        override fun createFromParcel(parcel: Parcel): Student {
+            return Student(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Student?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
